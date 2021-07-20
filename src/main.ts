2d2b00d36df7f592acabd648bb236fd98b9a5f27
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-07-20 07:13:38 +0530

    chore: configure dev environment

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const options = new DocumentBuilder()
@@ -17,6 +18,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT||3000);
 }
 bootstrap();

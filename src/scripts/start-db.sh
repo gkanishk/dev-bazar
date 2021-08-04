a3309f0f82bc53ad6600114915c59febf51fba1c
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-08-04 09:25:54 +0530

    docs: Updated example env

--- src/scripts/start-db.sh
+++ src/scripts/start-db.sh
@@ -3,7 +3,7 @@ set -e
 
 SERVER="127.0.0.1";
 PW="postgresh";
-DB="devbazartwos";
+DB="devbazar";
 
 echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of [$SERVER]"
 (docker kill $SERVER || :) && \

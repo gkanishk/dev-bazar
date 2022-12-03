041015c23294719292c144dc11955d39f25a3eed
Kanishk Gupta <kanishkgupta1234@gmail.com>
2022-12-03 12:14:38 +0530

    fix cors vercel

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,16 @@ import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://gkanishk-devbazar.vercel.app'],
-    methods: 'OPTIONS, DELETE, POST, GET, PATCH, PUT',
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'https://gkanishk-devbazar.vercel.app'],
+  //   methods: 'OPTIONS, DELETE, POST, GET, PATCH, PUT',
+  //   credentials: true
+  // });
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
   });
   app.useGlobalFilters(new HttpExceptionFilter());
 

f3918fdee7d552785cc867904f9db1822825e9c3
Kanishk Gupta <kanishkgupta1234@gmail.com>
2022-12-03 11:59:04 +0530

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:3000', 'https://gkanishk-devbazar.vercel.app'],
+    methods: 'OPTIONS, DELETE, POST, GET, PATCH, PUT',
   });
   app.useGlobalFilters(new HttpExceptionFilter());
 
